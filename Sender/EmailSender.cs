79cb65e6166e8a07e877efefc489fd538c19bd7c
Beytullah BÜYÜKTOLU <hi@beytullah.net>
2024-08-06 22:49:07 +0300

    added project

---
+++ Sender/EmailSender.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Mail;
+using System.Net;
+
+namespace Sender;
+public class EmailSender
+{
+    private readonly IConfiguration _configuration;
+    public EmailSender(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public void SendEmail(string toEmail, string subject, string body)
+    {
+        var emailSettings = _configuration.GetSection("EmailSettings");
+
+        using (var client = new SmtpClient(emailSettings["Host"], int.Parse(emailSettings["Port"])))
+        {
+            client.Credentials = new NetworkCredential(emailSettings["Username"], emailSettings["Password"]);
+            client.EnableSsl = bool.Parse(emailSettings["EnableSSL"]);
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(emailSettings["Username"], emailSettings["SenderName"]),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+            mailMessage.To.Add(toEmail);
+            client.Send(mailMessage);
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Sender;
+class Program
+{
+    public static IConfiguration _configuration { get; set; }
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        _configuration = builder.Build();
+        EmailSender emailSender = new EmailSender(_configuration);
+        emailSender.SendEmail("hi@beytullah.net", "Test Konusu", "Bu bir test mailidir.");
+        Console.WriteLine("Mail gönderildi.");
+    }
+}
